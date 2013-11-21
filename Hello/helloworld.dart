8ec5b081961cfe944bb76f48955ddd34b9520911
Irene <irene@aurametrix.com>
2013-11-21 14:53:01 -0800

    hello world

---
+++ Hello/helloworld.dart
@@ -0,0 +1,11 @@
+/**
+ * Angular is client-side structural framework for creating dynamic web applications
+ * It helps to extend HTML syntax to create custom elements
+ */
+
+import 'package:angular/angular.dart';
+
+main() {
+/// ront-end framework for easier web development
+  ngBootstrap();
+}

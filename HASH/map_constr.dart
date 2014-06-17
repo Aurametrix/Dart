43130ca302674b5ccea94184c30bd59d4cc346f1
Irene <irene@aurametrix.com>
2014-06-16 22:19:53 -0700

    created using a map constructor : ' :

---
+++ HASH/map_constr.dart
@@ -0,0 +1,9 @@
+var gifts = new Map();
+gifts['first'] = 'partridge';
+gifts['second'] = 'turtledoves';
+gifts['fifth'] = 'golden rings';
+
+var nobleGases = new Map();
+nobleGases[2] = 'helium';
+nobleGases[10] = 'neon';
+nobleGases[18] = 'argon';

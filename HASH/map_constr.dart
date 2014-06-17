9b03442d283ca5fc459bab5a486ccfb204d5f68b
Irene <irene@aurametrix.com>
2014-06-16 22:21:30 -0700

    adding key-value pairs and retrieving a value as in js

--- HASH/map_constr.dart
+++ HASH/map_constr.dart
@@ -7,3 +7,10 @@ var nobleGases = new Map();
 nobleGases[2] = 'helium';
 nobleGases[10] = 'neon';
 nobleGases[18] = 'argon';
+
+
+var gifts = { 'first': 'partridge' };
+gifts['fourth'] = 'calling birds';    // Add a key-value pair
+
+var gifts = { 'first': 'partridge' };
+assert(gifts['first'] == 'partridge');

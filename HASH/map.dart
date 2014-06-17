fa0568697ab6fe5ca1410a748fad545e6bc76060
Irene <irene@aurametrix.com>
2014-06-16 22:17:58 -0700

    created using map literals:

---
+++ HASH/map.dart
@@ -0,0 +1,13 @@
+var gifts = {
+// Keys       Values
+  'first'  : 'partridge',
+  'second' : 'turtledoves',
+  'fifth'  : 'golden rings'
+};
+
+var nobleGases = {
+// Keys  Values
+   2  : 'helium',
+   10 : 'neon',
+   18 : 'argon',
+};

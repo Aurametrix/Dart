2ee77a8045b56fc674efd14a80a441149d2391a6
Aurametrix <irene@aurametrix.com>
2016-10-19 22:23:12 -0700

    maximum value in a provided set of values

---
+++ HASH/greatest.dart
@@ -0,0 +1,11 @@
+/*This function returns the greatest element in a list of numbers */
+num findGreatestElement(List<num> list){
+  num greatestElement = list[0];
+  for (num element in list){
+    if (element>greatestElement) {
+      greatestElement = element;
+    }
+  }  
+  return greatestElement;
+}
+ 
